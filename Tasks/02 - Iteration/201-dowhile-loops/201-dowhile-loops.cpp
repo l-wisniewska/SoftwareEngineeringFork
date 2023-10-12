90fb2626d2f2c4defccda4a30e5c48144eff6ace
Lena Wisniewska <146249098+l-wisniewska@users.noreply.github.com>
2023-10-12 09:04:10 +0100

    Lab 2

--- Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
+++ Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
@@ -2,17 +2,22 @@
 
 int main()
 {
-    int i = 0;
+    int i = 5;
 
     printf("Start while loop\n");
 
     do
     {
         printf("i = %d\n", i);
-        i = i + 1;              // Alternatively, write i++;
-    } while (i < 5);            //Repeat if condition is met
+        i = i - 1;              // Alternatively, write i++;
+    } while (i >= 0);            //Repeat if condition is met
 
     printf("Finished: i = %d\n", i);
 }
 
+//program looped 5 times from 0-4
+//using while i < 5, the end value of i is 5
+//when using i < 6, program loops 5 times, prints i = 6
+
+//using i = 5, while i >= 0 program loops 5 times, prints i = -1
 
